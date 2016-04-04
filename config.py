36d89f176f380e69c0e5fde4820baa007bbf0d27
aljun <salamer_gaga@163.com>
2016-04-04 22:28:31 +0800

    ~~

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 #start_url="http://bbs.gter.net/forum.php?mod=forumdisplay&fid=49&typeid=158&filter=typeid&typeid=158&page="
-start_url="http://bbs.gter.net/forum.php?mod=forumdisplay&fid=486&typeid=992&filter=typeid&typeid=992&page=1"
+start_url="http://bbs.gter.net/forum.php?mod=forumdisplay&fid=486&typeid=992&filter=typeid&typeid=992&page="
 page_number=range(1,32)
 db_name='new_england_data'
