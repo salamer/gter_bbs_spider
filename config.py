11c608ea83ba1ed370a585e937190f5b9924fc20
aljun <salamer_gaga@163.com>
2016-04-04 22:12:56 +0800

    ~~

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 #start_url="http://bbs.gter.net/forum.php?mod=forumdisplay&fid=49&typeid=158&filter=typeid&typeid=158&page="
 start_url="http://bbs.gter.net/forum.php?mod=forumdisplay&fid=486&typeid=992&filter=typeid&typeid=992&page=1"
 page_number=range(1,32)
-db_name='england_data'
+db_name='new_england_data'
